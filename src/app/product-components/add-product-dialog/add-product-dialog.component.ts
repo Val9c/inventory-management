bc93dd5d315bde25f0b0a2cf032953383e030b80
Val9c <62969329+Val9c@users.noreply.github.com>
2024-11-25 13:59:58 +0100

    Modification ecran d'accueil et ajout de 3 nouveau composant

---
+++ src/app/product-components/add-product-dialog/add-product-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-product-dialog',
+  templateUrl: './add-product-dialog.component.html',
+})
+export class AddProductDialogComponent {
+}
